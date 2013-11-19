59a3409d0beca6e50fe67a88a23b26b127c5e2f9
vilal <lalandev@free.fr>
2013-11-19 21:31:30 +0100

    Mise en place du template (commenté pour le moment) + Erreur : ne comprend pas la fonction DotheImportThing de fichier.hpp

--- main.cpp
+++ main.cpp
@@ -5,14 +5,79 @@
 #include "Participants/adversaireParticipant.hpp"
 #include "Participants/joueurParticipant.hpp"
 
+#include <SDL/SDL.h>
+#include <GL/glew.h>
 #include <iostream>
+#include <cstdlib>
+
+#include <assimp/Importer.hpp> // C++ importer interface
+#include <assimp/scene.h> // Output data structure
+#include <assimp/postprocess.h> // Post processing flags
+
+static const Uint32 FPS = 30;
+static const Uint32 FRAME_DURATION = 1000.f / FPS;
+
+static const Uint32 WINDOW_WIDTH = 800;
+static const Uint32 WINDOW_HEIGHT = 600;
+static const Uint32 WINDOW_BPP = 32;
+
+int main() {
+	/*if(-1 == SDL_Init(SDL_INIT_VIDEO)) {
+		std::cerr << "Unable to initialize SDL" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	if(NULL == SDL_SetVideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_BPP, SDL_OPENGL)) {
+		std::cerr << "Unable to open the window and get an OpenGL context" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	SDL_WM_SetCaption("OpenGL4Imacs", NULL);
+
+	GLenum glewCode = glewInit();
+	if(GLEW_OK != glewCode) {
+		std::cerr << "Unable to initialize GLEW : " << glewGetErrorString(glewCode) << std::endl;
+		return EXIT_FAILURE;
+	}
 
 
-int main(int argc, char **argv) {
+	bool done = false;
+	while(!done) {
+		Uint32 tStart = SDL_GetTicks();
 
-	
+		// Rendering code goes here
 
+		// Application code goes here
 
+		SDL_Event e;
+		while(SDL_PollEvent(&e)) {
+			switch(e.type) {
+				default:
+					break;
+				case SDL_QUIT:
+					done = true;
+					break;
+			}
+		}
 
+		// Mise à jour de la fenêtre (synchronisation implicite avec OpenGL)
+		SDL_GL_SwapBuffers();
+
+		Uint32 tEnd = SDL_GetTicks();
+		Uint32 d = tEnd - tStart;
+		if(d < FRAME_DURATION) {
+			SDL_Delay(FRAME_DURATION - d);
+		}
+	}
+
+	SDL_Quit();
+*/
+	/*const aiScene* scene;
+	const std::string String = "Fichier/bird_final.md2";
+	const std::string& refString = String; 
+	bool wesh =  DoTheImportThing(refString);
+
+	return 1;
+*/
 	return EXIT_SUCCESS;
 }
