3a02426d273c8135856ae1a8c75d570dbb0f6259
vilal <lalandev@free.fr>
2013-11-19 21:28:16 +0100

    Ajout de la bibliothèqye assimp dans third-party

---
+++ third-party/assimp--3.0.1270-sdk/tools/assimp_cmd/CMakeFiles/assimp_cmd.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/vivie/Documents/Musikart/musikart/third-party/assimp--3.0.1270-sdk/tools/assimp_cmd/CompareDump.cpp" "/home/vivie/Documents/Musikart/musikart/third-party/assimp--3.0.1270-sdk/tools/assimp_cmd/CMakeFiles/assimp_cmd.dir/CompareDump.cpp.o"
+  "/home/vivie/Documents/Musikart/musikart/third-party/assimp--3.0.1270-sdk/tools/assimp_cmd/Export.cpp" "/home/vivie/Documents/Musikart/musikart/third-party/assimp--3.0.1270-sdk/tools/assimp_cmd/CMakeFiles/assimp_cmd.dir/Export.cpp.o"
+  "/home/vivie/Documents/Musikart/musikart/third-party/assimp--3.0.1270-sdk/tools/assimp_cmd/ImageExtractor.cpp" "/home/vivie/Documents/Musikart/musikart/third-party/assimp--3.0.1270-sdk/tools/assimp_cmd/CMakeFiles/assimp_cmd.dir/ImageExtractor.cpp.o"
+  "/home/vivie/Documents/Musikart/musikart/third-party/assimp--3.0.1270-sdk/tools/assimp_cmd/Info.cpp" "/home/vivie/Documents/Musikart/musikart/third-party/assimp--3.0.1270-sdk/tools/assimp_cmd/CMakeFiles/assimp_cmd.dir/Info.cpp.o"
+  "/home/vivie/Documents/Musikart/musikart/third-party/assimp--3.0.1270-sdk/tools/assimp_cmd/Main.cpp" "/home/vivie/Documents/Musikart/musikart/third-party/assimp--3.0.1270-sdk/tools/assimp_cmd/CMakeFiles/assimp_cmd.dir/Main.cpp.o"
+  "/home/vivie/Documents/Musikart/musikart/third-party/assimp--3.0.1270-sdk/tools/assimp_cmd/WriteDumb.cpp" "/home/vivie/Documents/Musikart/musikart/third-party/assimp--3.0.1270-sdk/tools/assimp_cmd/CMakeFiles/assimp_cmd.dir/WriteDumb.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "ASSIMP_BUILD_NO_OWN_ZLIB"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/vivie/Documents/Musikart/musikart/third-party/assimp--3.0.1270-sdk/code/CMakeFiles/assimp.dir/DependInfo.cmake"
+  )
