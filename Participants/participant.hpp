443aca08c922a83afb15c7b130e56beadb547e9b
vilal <lalandev@free.fr>
2013-12-03 19:35:30 +0100

    Création struct participant

--- Participants/participant.hpp
+++ Participants/participant.hpp
@@ -1,1 +1,14 @@
 #pragma once
+#include "../Voiture/voiture.hpp"
+
+struct Participant {
+	public:
+		virtual ~Participant(){}
+		virtual int retournerPremierBonus() const = 0;
+		virtual Voiture getVoiture()const;
+		virtual Voiture setVoiture()const;
+
+	private:
+		int* listeBonus;
+		Voiture voiture;
+};
