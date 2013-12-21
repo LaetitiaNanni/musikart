59bd50fc3ac8c0d68849857d2e9251178a0a3f70
Elena Loyatho <elena.loyatho@gmail.com>
2013-12-21 19:28:46 +0100

    voiture avec constructeurs

--- Voiture/voiture.cpp
+++ Voiture/voiture.cpp
@@ -1,1 +1,63 @@
 #include "voiture.hpp"
+
+
+///////////////////
+// Constructeurs
+///////////////////
+Voiture::Voiture(int vitesse, Point3D position, int puissance, int virage, int poids) :
+vitesse(vitesse), position(position), puissance(puissance), virage(virage),poids(poids)
+{}
+
+Voiture::Voiture(const Voiture& other) :
+vitesse(other.vitesse), position(other.position), puissance(other.puissance), virage(other.virage),poids(other.poids)
+{}
+
+
+//Destructeur
+//Voiture::~Voiture();
+
+
+///////////////////
+// Getteurs
+///////////////////
+int Voiture::getVitesse(){
+	return vitesse;
+}
+Point3D Voiture::getPosition(){
+	return position;
+}
+int Voiture::getPuissance(){
+	return puissance;
+}
+int Voiture::getVirage(){
+	return virage;
+}
+int Voiture::getPoids(){
+	return poids;
+}
+
+
+///////////////////
+// Setteurs
+///////////////////
+void Voiture::setVitesse(int vit) {
+	vitesse = vit;
+}
+
+void Voiture::setPosition(Point3D pos){
+	position= pos;
+}
+void Voiture::setPosition(int x, int y, int z){
+	position.x=x;
+	position.y=y;
+	position.z=z;	
+}
+void Voiture::setPuissance(int pui){
+	puissance = pui;
+}
+void Voiture::setVirage(int vir){
+	virage= vir;
+}
+void Voiture::setPoids(int poi){
+	poids= poi;
+}
--- Voiture/voiture.hpp
+++ Voiture/voiture.hpp
@@ -1,1 +1,37 @@
 #pragma once
+
+#include "../lib/include/Point3D.hpp"
+
+class Voiture {
+	private:
+		int vitesse;
+		Point3D position;
+		int puissance;
+		int virage;
+		int poids;
+	public:
+		// Constructeurs
+		Voiture(int vitesse, Point3D position, int puissance, int virage, int poids);
+		//Voiture(); //par défaut -> pas de sens ici je pense
+		Voiture(const Voiture& other); // constructeur par copie
+
+		//Destructeur
+		//~Voiture();
+
+		// Getteur
+		int getVitesse();
+		Point3D getPosition();
+		int getPuissance();
+		int getVirage();
+		int getPoids();
+
+		// Setteur
+		void setVitesse(int vit);
+		void setPosition(Point3D pos);
+		void setPosition(int x, int y, int z);
+		void setPuissance(int pui);
+		void setVirage(int vir);
+		void setPoids(int poi);
+
+		// Scanner : dans un rayon autour de lui il vérfie s'il y a l'autre objet passé en paramètre
+};
