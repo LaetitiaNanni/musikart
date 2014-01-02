c08dba6c9635c7120ced595db06056ca0ef44ad6
Elena Loyatho <elena.loyatho@gmail.com>
2014-01-02 11:37:55 +0100

    creation de l'héritage guitare

--- Voiture/guitareVoiture.cpp
+++ Voiture/guitareVoiture.cpp
@@ -1,2 +1,1 @@
-#include "voiture.hpp"
 #include "guitareVoiture.hpp"
--- Voiture/guitareVoiture.hpp
+++ Voiture/guitareVoiture.hpp
@@ -1,1 +1,10 @@
 #pragma once
+
+#include "voiture.hpp"
+
+class GuitareVoiture: public Voiture{
+	private:
+	
+	public:
+		// ici fonctions spécifiques à la guitare
+};
